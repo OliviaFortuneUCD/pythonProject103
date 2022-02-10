132544bec0aa2046ee06ca59fa4023793abc1e9c
OliviafortuneUCD <Test@test.com>
2022-02-10 17:52:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import matplotlib
+import matplotlib.pyplot as plt
+years=[1950,1955,1960,1965,1970,1980,1985,1990,1995,2000,2005,2010,2015]
+pops=[2.5,2.7,3.0,3.3,3.6,4.0,4.4,4.8,5.3,5.7,6.1,6.5,7.3]
+plt.plot(years,pops,color=(255/255,100/255,100/255))
+plt.ylabel("Population in Billions")
+plt.xlabel("Population growth by year")
+plt.title("Population Growth")
+plt.show
